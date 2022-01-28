5208ba43479b7e627000afad527dbecae07dce45
VaqueroFontenla <loret.vaquero@gmail.com>
2022-01-28 16:00:36 +0100

    Get all geatures checked at submit form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
+const API_URL = "https://caligari-api.herokuapp.com/api/v1";
 const form = document.form;
+const featuresWrapper = document.querySelector(".tags__wrapper");
+
+const getFeatures = () => {
+  fetch(`${API_URL}/features`)
+    .then((response) => response.json())
+    .then((features) => {
+      const tpl = features
+        .map(
+          (feature) => `
+        <li>
+          <input id=${feature.id} name=${feature.id} type="checkbox"/>
+          <label class="tag" for=${feature.id}>${feature.name}</label>
+        </li>
+        `
+        )
+        .join("");
+      featuresWrapper.innerHTML = tpl;
+    });
+};
 
 form.addEventListener("submit", (e) => {
   const name = form.name.value;
   const city = form.city.value;
-  console.log({ name, city });
+  const features = [
+    ...document.querySelectorAll('input[type="checkbox"]:checked'),
+  ].map((feature) => feature.id);
+  console.log({ name, city, features });
   e.preventDefault();
 });
+
+getFeatures();
