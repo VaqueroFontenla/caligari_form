a00d317c4357921c79353e0e688b71b0f6517a1f
Loreto <loreto@guud.tv>
2023-07-04 19:22:14 +0200

    :see_no_evil: delete gitignore

---
+++ config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB0LwoM2YMJp52WgwiADscdog9RqizdmvA',
+  authDomain: 'caligari-9b711.firebaseapp.com',
+  projectId: 'caligari-9b711',
+  storageBucket: 'caligari-9b711.appspot.com',
+  appId: 'caligari_project',
+}
